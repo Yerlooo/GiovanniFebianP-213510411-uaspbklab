66e5349f3d9f9686e0375c63a82dc8b62ee5f3c4
giovannifebian <gvani510@gmail.com>
2023-07-05 12:06:29 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/location" class="nav-link" :class="{ active: $route.path === '/location' }">Location</router-link>
+        </li>
+        <li>
+          <router-link to="/stopwatch" class="nav-link" :class="{ active: $route.path === '/stopwatch' }">Stopwatch</router-link>
+        </li>
+        <li>
+          <router-link to="/weather" class="nav-link" :class="{ active: $route.path === '/weather' }">Weather</router-link>
+        </li>
+        <li>
+          <router-link to="/tictactoe" class="nav-link" :class="{ active: $route.path === '/tictactoe' }">Tic Tac Toe</router-link>
+        </li>
+        <li>
+          <router-link to="/photo" class="nav-link" :class="{ active: $route.path === '/photo' }">Photo</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&family=Quicksand&family=Ubuntu+Condensed&display=swap");
+
+.app {
+  font-family: "Quicksand", sans-serif;
+  display: grid;
+  justify-items: center;
+  background: linear-gradient(to bottom right, #ed0000, #110def);
+}
+
+nav {
+  background-color: #f2f2f2;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+nav ul li {
+  margin-right: 10px;
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: #333;
+  padding: 5px;
+  transition: all 0.3s ease;
+}
+
+nav ul li a:hover {
+  transform: scale(1.1);
+  color: #555;
+  background-color: pink;
+}
+
+nav ul li a:active {
+  color: #777;
+  transform: scale(0.9);
+  transition: transform 0.2s ease;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+}
+
+.nav-link {
+  border: none;
+  border-radius: 5px;
+  transition: color 0.3s ease, transform 0.3s ease;
+}
+
+.nav-link:hover {
+  color: #fff;
+  background: linear-gradient(to bottom right, #ed0000, #110def);
+  transform: scale(1.1);
+}
+
+.nav-link:active {
+  color: rgb(0, 0, 0);
+  transform: scale(1.1);
+}
+
+.active {
+  color: #fff;
+  transform: scale(0.95);
+  background: linear-gradient(to bottom right, #ed0000, #110def);
 }
 </style>
---
+++ src/components/TicTacToeWidget.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <h2>Tic Tac Toe Widget</h2>
+    <div class="board">
+      <div v-for="(cell, index) in board" :key="index" class="cell" @click="makeMove(index)">
+        {{ cell }}
+      </div>
+    </div>
+    <button @click="resetBoard">Reset</button>
+    <p v-if="!gameOver">{{ status }}</p>
+    <p v-else>Game Over!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      board: Array(9).fill(""),
+      currentPlayer: "X",
+      gameOver: false,
+    };
+  },
+  computed: {
+    status() {
+      return `Player ${this.currentPlayer}'s turn`;
+    },
+  },
+  methods: {
+    makeMove(index) {
+      if (!this.board[index] && !this.gameOver) {
+        this.board.splice(index, 1, this.currentPlayer);
+        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+        this.checkWinner();
+      }
+    },
+    checkWinner() {
+      const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      for (let combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+          this.gameOver = true;
+          setTimeout(() => {
+            alert(`Player ${this.board[a]} wins!`);
+          }, 100);
+          return;
+        }
+      }
+
+      if (this.board.every((cell) => cell)) {
+        this.gameOver = true;
+        setTimeout(() => {
+          alert("Draw Game!");
+        }, 100);
+      }
+    },
+    resetBoard() {
+      this.board = Array(9).fill("");
+      this.currentPlayer = "X";
+      this.gameOver = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+.board {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+.cell {
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  border: 1px solid black;
+  cursor: pointer;
+  font-size: 24px;
+}
+
+.cell:hover {
+  background-color: #797979;
+}
+
+button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #2196f3;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #1976d2;
+}
+
+button:active {
+  background-color: #1565c0;
+}
+
+button:focus {
+  outline: none;
+}
+</style>
